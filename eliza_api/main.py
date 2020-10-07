062ebb5f4d771197d812fe8b5ecbaedaafc991b7
ulme <ulme@kth.se>
2020-10-07 19:49:06 +0200

    remove duplicate func and minor linting

--- eliza_api/main.py
+++ eliza_api/main.py
@@ -3,40 +3,30 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from eliza import Eliza
 
+
 class Item(BaseModel):
     text: str
+
 
 app = FastAPI()
 eliza = Eliza()
 eliza.load('doctor.txt')
 
-def read_root():
-    if not True:
-        response = eliza.getInitial()
-    else:
-        print("inside else")
-        response = eliza.runFromApi("I really need a friend")
-    initial = True
-
-    return response
 
 @app.get("/")
-
 def read_root():
     eliza = Eliza()
     eliza.load('doctor.txt')
-    if not True:
-        response = eliza.getInitial()
-    else:
-        print("inside else")
-        response = eliza.runFromApi("I really need a friend")
-    initial = True
-
+    # response = eliza.getInitial()
+    response = eliza.runFromApi("I really need a friend")
+    # initial = True
     return response
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
 
 @app.post("/eliza")
 async def create_item(item: Item):
