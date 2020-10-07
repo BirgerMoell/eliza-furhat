2aac44924b1ffac5f6e0fe0230726e6326093214
Birger Moell <birger.moell@gmail.com>
2020-10-07 20:40:25 +0200

    Cleanup

--- eliza_api/main.py
+++ eliza_api/main.py
@@ -3,69 +3,11 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from eliza import Eliza
 
-
 class DialogRequest(BaseModel):
     responseId: str
     queryResult: dict
     originalDetectIntentRequest: dict
     session: str
-
-class DialogResponse(BaseModel):
-    fulfillmentText: str
-    fulfillmentMessages: list
-    source: str
-
-
-
-#   let responseJson =  {
-#     "fulfillmentText": jokeResponse,
-#     "fulfillmentMessages": [
-#         {
-#             "text": {
-#                 "text": [
-#                   jokeResponse
-#                 ]
-#             }
-#         }
-#     ],
-#     "source": ""
-# }
-
-#     {
-#   "responseId": "f487a623-3564-4850-9626-3f135cc1974b-fddac391",
-#   "queryResult": {
-#     "queryText": "I would like have all the codes",
-#     "action": "input.unknown",
-#     "parameters": {},
-#     "allRequiredParamsPresent": true,
-#     "outputContexts": [
-#       {
-#         "name": "projects/furhatoracle-ehcs/agent/sessions/4b925e3d-8b66-580b-9819-02b22db6a016/contexts/__system_counters__",
-#         "lifespanCount": 1,
-#         "parameters": {
-#           "no-input": 0,
-#           "no-match": 1
-#         }
-#       }
-#     ],
-#     "intent": {
-#       "name": "projects/furhatoracle-ehcs/agent/intents/3acec099-cbda-4cf8-bf8e-d5e43323e449",
-#       "displayName": "Default Fallback Intent",
-#       "isFallback": true
-#     },
-#     "intentDetectionConfidence": 1,
-#     "languageCode": "en"
-#   },
-#   "originalDetectIntentRequest": {
-#     "source": "DIALOGFLOW_CONSOLE",
-#     "payload": {}
-#   },
-#   "session": "projects/furhatoracle-ehcs/agent/sessions/4b925e3d-8b66-580b-9819-02b22db6a016"
-# }
-
-
-class Item(BaseModel):
-    text: str
 
 
 app = FastAPI()
